122c65af29a4b79613cf5bc5419961ec630141b5
ooiliying11@gmail.com <ooiliying11@gmail.com>
2022-04-08 23:21:44 +0800

    front end: add listing pg and detail pg

---
+++ FrontEnd/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FrontEnd.Models;
+using FrontEnd.ViewModels;
+
+namespace FrontEnd.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieTicketBookingContext _context;
+
+        public MoviesController(MovieTicketBookingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var movie = _context.Movies
+               .Where( o => o.IsDeleted != true )
+               .ToList();
+            return View( movie );
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movies == null)
+            {
+                return NotFound();
+            }
+
+            return View(movies);
+        }
+    }
+}
