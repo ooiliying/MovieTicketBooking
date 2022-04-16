f5e3ba0669aa5883e4bb05966ad226b2343fe324
ooiliying11@gmail.com <ooiliying11@gmail.com>
2022-04-16 08:45:27 +0800

    admin: create sale

---
+++ Admin/Controllers/SalesController.cs
@@ -0,0 +1,46 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Admin.Models;
+
+namespace Admin.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly MovieTicketBookingContext _context;
+
+        public SalesController(MovieTicketBookingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sales
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Payments.ToListAsync());
+        }
+
+        // GET: Sales/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payments = await _context.Payments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (payments == null)
+            {
+                return NotFound();
+            }
+
+            return View(payments);
+        }
+    }
+}
